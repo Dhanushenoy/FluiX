4f2e809c5a36b032e7c8f95019589bfe879fd73b
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2025-04-12 11:15:27 +0530

    --folder option for animate is required

--- fluix/cli.py
+++ fluix/cli.py
@@ -12,14 +12,14 @@ def main():
     #
     ###############################
     animate = subparsers.add_parser('animate', help='Create animations (gif/mp4) from images')
-    animate.add_argument('--folder', type=str, help='Folder with image frames. If not given, current directory is taken. \n Important!!! Needs the following format :\n pic.0000.png \n pic.0001.png \n pic.0002.png \n ... \n pic.xxxx.png')
+    animate.add_argument('--folder', required=True, type=str, help='Name of the folder with image frames. Important!!! The files in the directory require the following format :\n pic.0000.png \n pic.0001.png \n pic.0002.png \n ... \n pic.xxxx.png')
     animate.add_argument('--output', type=str, required=True, help='Output file (eg. out.mp4 or out.gif). Also takes the output directory as prefix.')
     animate.add_argument('--fps', type=int, default=30, help='Frames per second. Default is 30')
     animate.add_argument('--range', type=str, default=None, help='Frame ranges to use, in the form start:end (eg. 20:180)' )
     animate.add_argument('--quality', type=int, default=10, help='Render quality (1-10)')
     animate.add_argument('--codec', type=str, default='libx264', help="FFmpeg codec to use (eg. libx264)")
 
-    
+
 
     ###############################
     #
