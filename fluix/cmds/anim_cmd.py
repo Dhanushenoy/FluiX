7180b2c7297e890b172fa29af62341e0f5970b2b
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2025-05-05 14:43:19 +0530

    Function argument in separator lines

--- fluix/cmds/anim_cmd.py
+++ fluix/cmds/anim_cmd.py
@@ -1,7 +1,13 @@
 import os
 from fluix.core.animate import images_to_video
 
-def run_animate(folder, output_path, fps=30, frame_range=None, quality=10, codec='libx264'):
+def run_animate(folder: str,
+                output_path: str,
+                fps: int = 30,
+                frame_range = None,
+                quality: int = 10,
+                codec: str = 'libx264',
+                resize: str = "none"):
     print("Animating your frames....")
     images_to_video(
         input_dir=folder,
